74d2074e57647ddfefe62281da911018c8e7c608
gulernurdonmez <gule0153@stud.kea.dk>
2022-03-31 17:51:26 +0200

    minor changes

--- src/main/java/com/example/wishlist/Services/WishListService.java
+++ src/main/java/com/example/wishlist/Services/WishListService.java
@@ -1,5 +1,9 @@
 package com.example.wishlist.Services;
 
+import com.example.wishlist.repositories.UserHandler;
+
 public class WishListService {
+
+    UserHandler userHandler = new UserHandler();
 
 }
--- src/main/java/com/example/wishlist/WishListApplication.java
+++ src/main/java/com/example/wishlist/WishListApplication.java
@@ -1,8 +1,5 @@
 package com.example.wishlist;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class WishListApplication {
 
