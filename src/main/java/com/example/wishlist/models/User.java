65ff18a450ada944ff0e48565b98e86ad41f65fb
gulernurdonmez <gule0153@stud.kea.dk>
2022-03-31 10:47:20 +0200

    minor chances

--- src/main/java/com/example/wishlist/models/User.java
+++ src/main/java/com/example/wishlist/models/User.java
@@ -5,11 +5,21 @@ public class User {
     private int Id;
     private String password;
     private String email;
+    private String name;
 
-    public User(int id, String password, String email) {
+    public User(int id, String password, String email, String name) {
         Id = id;
         this.password = password;
         this.email = email;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getId() {
