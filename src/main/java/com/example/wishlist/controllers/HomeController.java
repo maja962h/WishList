eb188868364469855e129361ac79f1d66f8a858f
gulernurdonmez <gule0153@stud.kea.dk>
2022-03-31 10:50:30 +0200

    added getMappings

--- src/main/java/com/example/wishlist/controllers/HomeController.java
+++ src/main/java/com/example/wishlist/controllers/HomeController.java
@@ -1,4 +1,23 @@
 package com.example.wishlist.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/logIn")
+    public String logIn(){
+        return "logIn";
+    }
+
+    @GetMapping("/signUp")
+    public String signUp(){
+        return "signUp";
+    }
 }
